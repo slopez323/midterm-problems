8f0fe6aed815cfb30a789cb3a663c5f525df212d
Sarah Lopez <slopez323@gmail.com>
2022-03-04 11:56:56 -0500

    completed

--- main.js
+++ main.js
@@ -1,43 +1,78 @@
-function isEvenlyDivisible() {
+function isEvenlyDivisible(num1, num2) {
+  return num1 % num2 === 0;
 }
 
-function halfSquare() {
+function halfSquare(num) {
+  return num * num / 2;
 }
 
-function isLong() {
+function exclaim(str) {
+  if (str[str.length - 1] === '!') {
+    return exclaim(str.substr(0, str.length - 1))
+  } else return `${str}!`
 }
 
-function exclaim() {
+function isLong(str) {
+  return str.length >= 15
 }
 
-function countWords() {
+function containsDigit(str) {
+  str = [...str]
+  return str.some(c => !isNaN(+c) && c !== ' ')
 }
 
-function containsDigit() {
+// function containsDigit(str) {
+//   str = [...str]
+//   for (let i of str){
+//     if (!isNaN(+i) && i !== ' ') return true
+//   }
+//   return false
+// }
+
+function containsLowerCase(str) {
+  str = [...str]
+  return str.some(c => c === c.toLowerCase() && c.toUpperCase() !== c.toLowerCase())
 }
 
-function containsLowerCase() {
+function containsUpperCase(str) {
+  str = [...str]
+  return str.some(c => c === c.toUpperCase() && c.toUpperCase() !== c.toLowerCase())
 }
 
-function containsUpperCase() {
+function containsNonAlphanumeric(str) {
+  str = [...str]
+  return str.some(c => c.toUpperCase() === c.toLowerCase() && isNaN(+c) || c === ' ')
 }
 
-function containsNonAlphanumeric() {
+function containsSpace(str) {
+  str = [...str]
+  return str.some(c => c === ' ')
 }
 
-function containsSpace() {
+function countWords(str) {
+  str = str.split(' ')
+  return str.length
 }
 
-function digits() {
+function digits(num) {
+  num = Array.from(num.toString(), Number)
+  return num.filter(n => !isNaN(n))
 }
 
-function truncate() {
+function truncate(str) {
+  if (str.length >= 15) {
+    return `${str.substr(0, 8)}...`
+  } else return str
 }
 
-function isValidPassword() {
+function isValidPassword(str) {
+  return containsLowerCase(str) && containsUpperCase(str) && containsDigit(str) && containsNonAlphanumeric(str) && !containsSpace(str)
 }
 
-function onlyPunchy() {
+function onlyPunchy(titles) {
+  return titles
+    .map(title => exclaim(title))
+    .filter(title => title.length < 15)
 }
 
 
